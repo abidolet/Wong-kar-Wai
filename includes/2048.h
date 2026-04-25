6a354b5d0d047c3ebc25ac7c3881628e20bf5950
Alexis Bidolet--Foray <abidolet@z3r9p2.42lyon.fr>
2026-04-25 10:58:50 +0200

    rm color from logger

--- includes/2048.h
+++ includes/2048.h
@@ -8,6 +8,7 @@ typedef enum e_state
 	MENU = 0,
 	LEADERBOARD,
 	PLAYING,
+	WIN
 }	t_state;
 
 typedef struct s_game
@@ -19,9 +20,14 @@ typedef struct s_game
 	t_state	state;
 }	t_game;
 
-# define KEY_ESCAPE 27
+enum e_const
+{
+	WIN_VALUE = 2048,
+	KEY_ESCAPE = 27
+};
 
 void	draw(t_game *game);
 void	update(t_game *game);
+void	new_game(t_game *game);
 
 #endif
--- srcs/update.c
+++ srcs/update.c
@@ -1,6 +1,29 @@
 #include "2048.h"
 
+void	print_board(t_game *game)
+{
+	for (size_t i = 0; i < game->size; i++)
+	{
+		for (size_t j = 0; j < game->size; j++)
+		{
+			if (game->board[i][j] == 0)
+				ft_dprintf(2, ". ");
+			else
+				ft_dprintf(2, "%zu ", game->board[i][j]);
+		}
+		ft_dprintf(2, "\n");
+	}
+}
+
 void	update(t_game *game)
+{
+	ft_dprintf(2, "Score: %zu\n", game->score);
+	print_board(game);
+	ft_dprintf(2, "\n");
+	(void)game;
+}
+
+void	new_game(t_game *game)
 {
 	(void)game;
 }
