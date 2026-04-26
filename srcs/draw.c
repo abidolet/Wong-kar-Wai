eee746c53a5f99c9bbd1b07de74dd2d226507130
yamorett <yamorett@student.42lyon.fr>
2026-04-26 10:28:43 +0200

    display centering

--- srcs/draw.c
+++ srcs/draw.c
@@ -92,7 +92,7 @@ void	draw_single_cell(t_board *board, int i, int j, int start_x, int start_y, in
     }
 
     cell_y = start_y + i * (h + 1) + 1 + (h / 2);
-    cell_x = start_x + j * (l + 1) + 1;
+    cell_x = start_x + j * (l + 1) + 1 + ((l - num_len) / 2);
     mvprintw(cell_y, cell_x, "%zu", board->cells[i][j]);
 
     attroff(COLOR_PAIR(color_id));
